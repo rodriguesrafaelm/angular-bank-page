00210a13671d38e24e17ad03635df8552dc02a4f
rafaelmrodriguess <rafaelmrodrigues96@yahoo.com.br>
2022-11-03 17:54:13 -0300

    add attributes

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -7,6 +7,8 @@ import { Component } from '@angular/core';
 })
 export class NovaTransferenciaComponent{
 
+  valor: number;
+  destino: number;
 
   transferir(){
     console.log('Solicitado nova transferencia')
